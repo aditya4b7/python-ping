7e41e94b6431283f436dad8251cad4cbbcd15171
saijusgeorge <saijusgeorge1@gmail.com>
2019-10-03 14:43:58 +0300

    added new files

--- htmlconvertor.sh
+++ htmlconvertor.sh
@@ -11,7 +11,7 @@ echo "
     <th>Status</th>
   </tr>
 
-" > index.html
+" > $html
 
     for file in `ls *.log` ; do
 
@@ -19,23 +19,25 @@ source=`awk '{print $3}' $file`
 destination=`awk '{print $5}' $file`
 status=`awk '{print $6}' $file`
 
-if [ "$status" == "OK" ];then
+if [ $status == OK ]
+then
 echo "
   <tr>
     <td>$source</td>
     <td>$destination</td>
     <td bgcolor="#3CFF33">$status</td>
   </tr>
-" >> index.html
+" >> $html
 
-elif [ "$status" == "FAILED" ];then
+elif [ $status == FAILED ]
+then
 echo "
   <tr>
     <td>$source</td>
     <td>$destination</td>
     <td bgcolor="#FF4933">$status</td>
   </tr>
-" >> index.html
+" >> $html
 fi
 
     done
